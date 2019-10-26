bd0ab6c557fb285db26ff3a8d75f4f7b44c4e955
Ziyovuddin <z.shamsiddini@gmail.com>
2019-10-26 15:02:26 -0400

    tims palindrome problem and solutions

---
+++ Tim_palindrome/Palindrome3.java
@@ -0,0 +1,30 @@
+public class Palindrome3 {
+  public static void main(String[] args){
+    /*
+    // String number = "22345677654321";
+    // int middle = number.length() / 2;
+    */
+
+    int a = 12;
+    int b = 3;
+
+    System.out.println(a++);
+    System.out.println(++b);
+
+    /*
+    // for (int i = 0; i < middle; i++){
+    //   System.out.println(number.charAt(i) + " " + number.charAt(number.length() - i - 1));
+    //   if(number.charAt(i) != number.charAt(number.length() - i - 1)){
+    //     System.out.println("Not Palindrome");
+    //     break;
+    //   }
+    // }      
+    */  
+  }
+}
+
+
+
+
+
+                        
